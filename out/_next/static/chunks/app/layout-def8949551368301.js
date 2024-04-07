60d07f121db7d3adc937c49ce865af08cbe9c360
Lasha <lasha.lasha@gmail.com>
2024-04-08 01:52:46 +0400

    removed out folder from git ignore

---
+++ out/_next/static/chunks/app/layout-def8949551368301.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7079:function(n,e,u){Promise.resolve().then(u.t.bind(u,4930,23))},4930:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=7079)}),_N_E=n.O()}]);
